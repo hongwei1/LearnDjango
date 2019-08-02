4073b0c6a326e76feb188a49d969210229212941
hongwei1 <hongwei@tesobe.com>
2019-08-02 15:28:54 +0200

    refactory the views and urls.
    (cherry picked from commit 94e665f01fcf70d66ee5029ca140a6e6bc4dd4c2)

--- polls/urls.py
+++ polls/urls.py
@@ -1,40 +1,42 @@
 from django.urls import path
 
-from . import views
+from django.urls import path
 
+from . import views
 app_name = 'polls' # this is a Namespaing URL names. 这个 URL 有命名空间的.可以通过这个调用下面的 views
 
 # 开始重构,改良代码
-# urlpatterns = [
-#     path('', views.IndexView.as_view(), name='index'),
-#     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-#     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-#     path('<int:question_id>/vote/', views.vote, name='vote'),
-# ]
-
-
 urlpatterns = [
-    # this is a path from `django.urls`, URLconf stuff.
-    # the path is passed 4 arguments:
-    # route  -> string, contains a URL pattern.
-    # view   -> it is the function, with HttpRequest object as first argument, and any arguments.
-    # kwargs -> Arbitrary keyword arguments can be passed in a dirctionry to the target view.
-    # name   -> Naming your URL lets you refer to it unambiguously from elsewhere in Django.
-    # ex: /polls/
-    path('', views.index, name="index"),
-    # path('', views.index, name="index"),
-
-    # :question_id> --> defines the name that will be used to identify the matched pattern
-    # <int: --> part is a converter that dermines what patterns should mathc this part of he URL path
-    # no need cruft, such as .html, unless you want to.
-    # ex: /polls/5/ --> 司机说.view.detail_old 就是 python 的函数,函数的参数是通过这一步实现的.参数来自与 URL.
-    path('<int:question_id>/', views.detail, name='detail'),
-    # path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    # IndexView 是继承自ListView. 所以用 as_view 调用就行了.不能调用自己的.
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-
-    # ex:/poll/mineOwnPratice/
-    path('mineOwnPratice', views.mineOwnPratice, name="mineOwnPratice"),
 ]
+
+#Old Style,没有模板,没有道路
+# urlpatterns = [
+#     # this is a path from `django.urls`, URLconf stuff.
+#     # the path is passed 4 arguments:
+#     # route  -> string, contains a URL pattern.
+#     # view   -> it is the function, with HttpRequest object as first argument, and any arguments.
+#     # kwargs -> Arbitrary keyword arguments can be passed in a dirctionry to the target view.
+#     # name   -> Naming your URL lets you refer to it unambiguously from elsewhere in Django.
+#     # ex: /polls/
+#     path('', views_old.index, name="index"),
+#     # path('', views.index, name="index"),
+#
+#     # :question_id> --> defines the name that will be used to identify the matched pattern
+#     # <int: --> part is a converter that dermines what patterns should mathc this part of he URL path
+#     # no need cruft, such as .html, unless you want to.
+#     # ex: /polls/5/ --> 司机说.view.detail_old 就是 python 的函数,函数的参数是通过这一步实现的.参数来自与 URL.
+#     path('<int:question_id>/', views_old.detail, name='detail'),
+#     # path('<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results/
+#     path('<int:question_id>/results/', views_old.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:question_id>/vote/', views_old.vote, name='vote'),
+#
+#     # ex:/poll/mineOwnPratice/
+#     path('mineOwnPratice', views_old.mineOwnPratice, name="mineOwnPratice"),
+# ]
--- polls/views.py
+++ polls/views.py
@@ -1,136 +1,37 @@
-# https://docs.djangoproject.com/en/2.0/intro/tutorial01/#write-your-first-view
-# 每一个 view 只做两件事: 返回一个请求页面内容的 HttpResponse 对象. 或者抛出一个异常.Http404.
-#
-from typing import Dict
-
-from django.db.models import QuerySet
-from django.http import HttpResponse
-from django.template import loader
-
-#This is callback function, need map this to a URL. and return HttpResponse. 
-#to call the view, we need to map it to a URL - add for this we need a URLconf.
-#you need create the `/Users/zhanghongwei/Documents/GitHub-Tower/LearnDjango/polls/urls.py` 
-
-# 这个html 内容和样子集成了. 不能区分, 要用 Templates 系统, 分离页面设计和内容...
-def index_old_1(request):
-    lasest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output: str = ', '.join([q.question_text for q in lasest_question_list])
-    return HttpResponse(output) #就是普通的 String, 可以直接返回给 HttpResponse 到页面.
-
-# 用了 template 系统, 分离内容和样式,用 context 和 template 生成前端.
-def index_old_2(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:2]
-    template = loader.get_template('polls/index_old.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    # template 用 render 渲染 html要用context 和 request 两个 parameters.
-    return HttpResponse(template.render(context, request))
-
-# https://docs.djangoproject.com/zh-hans/2.2/intro/tutorial03/#a-shortcut-render
-# 载入模板, 填充上下文, 再返回由他生成的 HttpResponse对象. --> 快捷函数:render().
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    # render --> request+ template+ context are all in the same place, and it can show the return here.
-    # the context is just a Dict, it is an optional field.
-    return render(request, 'polls/index.html', context)
-
-
-from polls.models import Question, Choice
-from django.utils import timezone
-# https://docs.djangoproject.com/en/2.0/intro/tutorial02/#playing-with-the-api
-# I just want to have fun here, test some database apis.
-def mineOwnPratice(request):
-    current_year = timezone.now().year
-    a19 = Question.objects.all()
-    q= Question(question_text="What's new ?", pub_date=timezone.now())
-    q.save()
-    a22 = q.id
-    a23 = q.pub_date
-    a24 = q.question_text ="What's up?"
-    a25 = q.save()
-    a26 = Question.objects.all()
-    #Django provies a rich database lookup API that's enritely driven by keyword arguments
-    a28 = Question.objects.filter(id=1)
-    a29 = Question.objects.filter(question_text__startswith='What')
-    a30 = Question.objects.filter(pub_date__year=current_year)
-    
-    # shortcut for primary-key exact lookups.
-    a32 = Question.objects.get(pk =1)
-    a32.was_published_recently()
-    
-    #Foreign-Key, from Question -> Choice
-    a37 =  a32.choice_set.all()
-    a32.choice_set.create(choice_text="Not Much", votes=0)
-    a32.choice_set.create(choice_text="The Sky", votes=0)
-    a40: Choice = a32.choice_set.create(choice_text="Just Hacking again", votes=0)
-    a40.question
-    a32.choice_set.all()
-    a32.choice_set.count()
-    
-    
-    # The API automtically follows relationships as far as you need.
-    # Use double underscores (__) to separate relationships. works ,as many as you want
-    #eg: question.pub_date.year == question__pub_date__year in Django
-    a49 = Choice.objects.filter(question__pub_date__year=current_year)
-    
-    #Delete one of the choices
-    a52 = a32.choice_set.filter(choice_text__startswith="Just Hacking")
-    a52.delete()
-    
-    
-    
-    
-    
-    return HttpResponse(a32) 
-
-# the view only do two things: HttpResponse or an exception Http404.
-# detail_old will get the requeset and question_id from webpage.
-def detail_old(request, question_id ):
-    return HttpResponse("You are looking at question %s." % question_id)
-
-from django.http import Http404
-from django.shortcuts import render
-
-from .models import Question
-# ...
-def detail_old2(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist !!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    return render(request, 'polls/detail.html', {'question': question})
-
-from django.shortcuts import get_object_or_404, render
-from .models import Question
-# ...
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results_old(request, question_id):
-    response = "you are looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-from django.shortcuts import get_object_or_404, render
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
-def vote_old(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
-
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
-# ...
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    # As to ListView ,it will provide the default context = question_list.
+    # so we need manully define the context_object_name.
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    #question 是数据库表名字,会自动提供一个 context 变量给 template 用; It is question.
+    model = Question
+    #可以区分,DetailView 和ResultsView 是两个Html,但是相同的后端.
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    try: #request.POST是一个类字典对象, 让你可以通过关键字的名字获取提交的数据.--> request.POST['choice'] 以字符串返回 Choice 的 ID.
+    try:  # request.POST是一个类字典对象, 让你可以通过关键字的名字获取提交的数据.--> request.POST['choice'] 以字符串返回 Choice 的 ID.
         selected_choice: Choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
