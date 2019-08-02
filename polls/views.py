cf4961407cab5c637a68c275b6c1de681502acf5
hongwei1 <hongwei@tesobe.com>
2019-08-02 14:17:16 +0200

    added form

--- polls/views.py
+++ polls/views.py
@@ -109,9 +109,40 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
+def results_old(request, question_id):
     response = "you are looking at the results of question %s."
     return HttpResponse(response % question_id)
 
-def vote(request, question_id):
+from django.shortcuts import get_object_or_404, render
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote_old(request, question_id):
     return HttpResponse("You are voting on question %s." % question_id)
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Choice, Question
+# ...
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try: #request.POST是一个类字典对象, 让你可以通过关键字的名字获取提交的数据.--> request.POST['choice'] 以字符串返回 Choice 的 ID.
+        selected_choice: Choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # reserse()函数避免了,我们在视图中用硬编码 URL
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
